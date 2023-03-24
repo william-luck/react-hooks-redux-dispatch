88b632a7b4752b56fc2e17228b52d4232f283879
William Luck <luck.william.h@gmail.com>
2023-03-24 09:53:27 -0300

    commit tree a99ba9f575c6622769a534bbdea706f78f80c02b

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,6 @@
+let state = { count: 0 };
+
+// The reducer
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +10,22 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+// Dispatch, calls the reducer
+function dispatch(action) {
+  // Removing the need to store the new state in a variable outside this function. 
+  state = changeState(state, action)
+  // Updates count on page
+  render()
+}
 
-changeState(state, action);
+function render() {
+  const app = document.querySelector("#app");
+  app.textContent = state.count;
+}
+
+// Initial render on page
+render()
+
+
+// let state = { count: 0 };
+// let action = { type: "counter/increment" };
